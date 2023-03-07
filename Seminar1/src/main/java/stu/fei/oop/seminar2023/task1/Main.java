cefb03c559e447e64c1f1549ed3689668e656f6e
Matej Jurčík <xjurcikm@stuba.sk>
2023-03-07 16:23:25 +0100

    Added codes from previous seminars

---
+++ Seminar1/src/main/java/stu/fei/oop/seminar2023/task1/Main.java
@@ -0,0 +1,7 @@
+package stu.fei.oop.seminar2023.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Seminar1/src/main/java/stu/fei/oop/seminar2023/task2/Main.java
@@ -0,0 +1,17 @@
+package stu.fei.oop.seminar2023.task2;
+
+public class Main {
+    public static int compareTwoNumbers(int first, int second) {
+        if (first > second) {
+            return 1;
+        } else if ( first == second) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(compareTwoNumbers(2,3));
+    }
+}
