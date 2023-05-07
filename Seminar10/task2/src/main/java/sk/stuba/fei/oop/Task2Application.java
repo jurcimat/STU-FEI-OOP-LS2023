4387b4032d677030625b26b198103ef49a4df3d7
Matej Jurčík <xjurcikm@stuba.sk>
2023-05-07 18:37:35 +0200

    Seminar 10 - task 2

---
+++ Seminar10/task2/src/main/java/sk/stuba/fei/oop/Task2Application.java
@@ -0,0 +1,12 @@
+package sk.stuba.fei.oop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Task2Application.class, args);
+    }
+
+}
